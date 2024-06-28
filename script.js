248abba57cdf1e1183fa5fee851b81459c6d2c2b
Fasihi Ali <153727111+fasihi8298@users.noreply.github.com>
2024-06-28 13:02:51 +0500

    //Create dropdown from the currencies array

---
+++ script.js
@@ -0,0 +1,24 @@
+let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
+const fromDropDown = document.getElementById("from-currency-select");
+const toDropDown = document.getElementById("to-currency-select");
+
+//Create dropdown from the currencies array
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  fromDropDown.add(option);
+});
+
+//Repeat same thing for the other dropdown
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  toDropDown.add(option);
+});
+
+//Setting default values
+fromDropDown.value = "USD";
+toDropDown.value = "INR";
+
